a02b5f57332f4b2940d93cf4bb9576b3fbd086d9
Elberd Galaiev <elberdgalaiev@gmail.com>
2020-03-12 19:16:21 +0100

    fix the manager dashboard (now you can add, edit and delete items from the dashboard menu)

---
+++ StandApp/app/src/main/java/com/example/standapp/DashboardListViewAdapter.java
@@ -0,0 +1,61 @@
+package com.example.standapp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class DashboardListViewAdapter extends BaseAdapter {
+
+    LayoutInflater inflater;
+    List<ManagerDashboard.DashboardItem> items;
+
+    public DashboardListViewAdapter(Activity context, List<ManagerDashboard.DashboardItem> items) {
+        super();
+        this.items = items;
+        this.inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ManagerDashboard.DashboardItem  item = items.get(position);
+        View view = convertView;
+
+
+        if (convertView == null) {
+            view = inflater.inflate(R.layout.custom_product_listview, null);
+        }
+        ImageView icon = (ImageView) view.findViewById(R.id.icon_menu);
+        TextView title = (TextView) view.findViewById(R.id.textView_title);
+        TextView price = (TextView) view.findViewById(R.id.textView_price);
+
+        icon.setImageResource(item.icon);
+        title.setText(item.title);
+        price.setText(item.price);
+
+        return view;
+    }
+}
