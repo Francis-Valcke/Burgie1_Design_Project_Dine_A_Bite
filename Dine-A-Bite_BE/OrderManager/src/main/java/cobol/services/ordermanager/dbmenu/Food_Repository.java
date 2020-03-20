ad8a0c1ff7bacc73c57d15bf723014ddf58d24a2
WannesVL <wannes.vanleemput@ugent.be>
2020-03-20 16:26:23 +0100

    Modify Order to comply with Food class of menu handler

--- Dine-A-Bite_BE/OrderManager/src/main/java/cobol/services/ordermanager/dbmenu/Food_Repository.java
+++ Dine-A-Bite_BE/OrderManager/src/main/java/cobol/services/ordermanager/dbmenu/Food_Repository.java
@@ -17,4 +17,6 @@ public interface Food_Repository extends CrudRepository<Food, Integer> {
     @Query("select u from Food u " +
             "where u.brandname = ?1")
     List<Food> findByBrand(String standname);
+    @Query("select u from Food u where u.name=?1 and u.brandname=?2")
+    Food findByNameAndBrand(String name, String Brand);
 }
