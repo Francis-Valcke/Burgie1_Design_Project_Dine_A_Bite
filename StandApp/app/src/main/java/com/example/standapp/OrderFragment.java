e38914be3be7aa6e9eaacf5f6b7bcdb6f0936718
Elberd Galaiev <elberdgalaiev@gmail.com>
2020-03-29 23:07:27 +0200

    test a bug (Tim will check)

--- StandApp/app/src/main/java/com/example/standapp/OrderFragment.java
+++ StandApp/app/src/main/java/com/example/standapp/OrderFragment.java
@@ -4,32 +4,118 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ExpandableListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.standapp.order.CommonOrder;
+import com.example.standapp.order.CommonOrderItem;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class OrderFragment extends Fragment {
+
+    private Button refresh;
     private ExpandableListView listView;
     private ExpandableListAdapter listAdapter;
-    private List<String> listDataHeader;
-    private HashMap<String,List<String>> listHash;
-
+    private List<String> listDataHeader = new ArrayList<>();
+    private HashMap<String,List<String>> listHash = new HashMap<>();
+    //private int standId = 100;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_order, container, false);
 
+        refresh = view.findViewById(R.id.refresh);
         listView = view.findViewById(R.id.expandable_listview);
-        initData();
+        //initData();
+        listDataHeader = new ArrayList<>();
+        listHash = new HashMap<>();
         listAdapter = new ExpandableListAdapter(listDataHeader,listHash);
         listView.setAdapter(listAdapter);
+
+        refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Instantiate the RequestQueue
+                RequestQueue queue = Volley.newRequestQueue(getContext());
+                String url = "http://cobol.idlab.ugent.be:8093/registerSubscriber"; // TODO: fix url
+
+                //GET
+                final JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        /*if (response == null) {
+                            Toast mToast = Toast.makeText(getContext(), "No new orders", Toast.LENGTH_SHORT);
+                            mToast.show();
+                            return;
+                        }*/
+                        ObjectMapper mapper = new ObjectMapper();
+                        //Map<String, String> headers = jsonRequest.getHeaders();
+                        try {
+                            CommonOrder order = mapper.readValue(response.get("order").toString(), CommonOrder.class);
+                            String orderName = "Order#" + Integer.toString(order.getId());
+                            listDataHeader.add(orderName);
+                            List<String> order_details = new ArrayList<>();
+                            for (CommonOrderItem i: order.getOrderItems()) {
+                                String detail = Integer.toString(i.getAmount()) + " " + i.getFoodname();
+                                order_details.add(detail);
+                            }
+                            //order_details.add("2 Fries");
+                            //order_details.add("3 Cheeseburgers");
+
+                            listHash.put(orderName, order_details);
+                            listAdapter.notifyDataSetChanged();
+
+                        } catch (JsonProcessingException | JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                        Toast mToast = Toast.makeText(getContext(), error.toString(), Toast.LENGTH_SHORT);
+                        mToast.show();
+                    }
+                }) {
+                    @Override
+                    public Map<String, String> getHeaders() {
+                        HashMap<String, String> headers = new HashMap<>();
+                        headers.put("Content-Type", "application/json");
+                        headers.put("Authorization", "Bearer" + " " + "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmcmFuY2lzIiwicm9sZXMiOlsiUk9MRV9VU0VSIiwiUk9MRV9BRE1JTiJdLCJpYXQiOjE1ODQ2MTAwMTcsImV4cCI6MTc0MjI5MDAxN30.5UNYM5Qtc4anyHrJXIuK0OUlsbAPNyS9_vr-1QcOWnQ");
+                        return headers;
+                    }
+                };
+                //Add the request to the RequestQueue
+                queue.add(jsonRequest);
+            }
+        });
+
         return view;
     }
 
---
+++ StandApp/app/src/main/java/com/example/standapp/order/CommonOrder.java
@@ -0,0 +1,97 @@
+package com.example.standapp.order;
+
+import com.example.standapp.MenuItem;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class CommonOrder {
+
+    // unique id for this order
+    private int id;
+
+    private Calendar startTime;
+    private Calendar expectedTime;
+
+    private status orderStatus;
+
+    private int standId;
+    private String brandName;
+    private String standName;
+
+    //----- Request ------//
+    private List<CommonOrderItem> orderItems;
+
+    // Coordinates Attendee on moment that order was mad
+    private double latitude;
+    private double longitude;
+
+    public enum status {
+        SEND,
+        PENDING,
+        DECLINED,
+        CONFIRMED,
+        READY
+    }
+
+    public CommonOrder(List<MenuItem> menuItems, String standName, String brandName, double latitude, double longitude){
+        this.id=0;
+        this.latitude=latitude;
+        this.longitude=longitude;
+
+        this.standName=standName;
+        this.brandName=brandName;
+
+        this.startTime=Calendar.getInstance();
+        this.expectedTime=Calendar.getInstance();
+
+        this.orderStatus=status.SEND;
+
+        this.orderItems=new ArrayList<>();
+        for (MenuItem menuItem : menuItems) {
+            orderItems.add(new CommonOrderItem(menuItem.getFoodName(), menuItem.getCount()));
+        }
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public int computeRemainingTime(){
+        return (int) (expectedTime.getTimeInMillis()-startTime.getTimeInMillis());
+    }
+
+    public status getOrderStatus() {
+        return orderStatus;
+    }
+
+    public int getStandId() {
+        return standId;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public String getStandName() {
+        return standName;
+    }
+
+    public List<CommonOrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+
+
+
+}
