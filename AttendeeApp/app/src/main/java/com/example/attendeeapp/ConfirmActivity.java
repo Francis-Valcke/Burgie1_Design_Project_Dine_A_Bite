9dee0df71ed9c6d4cae0c3082721bb7d78b74ab0
Nathan <nathan.gistelinck@ugent.be>
2020-04-18 14:30:19 +0200

    Initiate migration to ordering from specific stand

--- AttendeeApp/app/src/main/java/com/example/attendeeapp/ConfirmActivity.java
+++ AttendeeApp/app/src/main/java/com/example/attendeeapp/ConfirmActivity.java
@@ -59,7 +59,6 @@ public class ConfirmActivity extends AppCompatActivity implements AdapterView.On
 
     private ArrayAdapter<String> standListAdapter;
     private Location lastLocation;
-    private Multimap<String, String> brandStandMap = ArrayListMultimap.create();
     private ArrayList<CommonFood> ordered;
     private int cartCount;
     private BigDecimal totalPrice;
@@ -81,7 +80,6 @@ public class ConfirmActivity extends AppCompatActivity implements AdapterView.On
         totalPrice = (BigDecimal) getIntent().getSerializableExtra("totalPrice");
         lastLocation = getIntent().getParcelableExtra("location");
         requestOrderRecommend();
-        fetchStandNames();
 
         // Make distance and time of recommendation invisible until recommendation comes available
         TextView distanceText = findViewById(R.id.recommend_distance_text);
@@ -109,6 +107,7 @@ public class ConfirmActivity extends AppCompatActivity implements AdapterView.On
         standListAdapter = new ArrayAdapter<>(this,
                 R.layout.stand_spinner_item, new ArrayList<String>());
         spinner.setAdapter(standListAdapter);
+        standListAdapter.add("No stands available");
 
         // Enable the Up button
         assert ab != null;
@@ -116,7 +115,7 @@ public class ConfirmActivity extends AppCompatActivity implements AdapterView.On
 
 
         // Handle choose own stand and choose recommended stand buttons
-        Button chooseStandButton = findViewById(R.id.button_confirm_stand);
+        /*Button chooseStandButton = findViewById(R.id.button_confirm);
         chooseStandButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -125,7 +124,7 @@ public class ConfirmActivity extends AppCompatActivity implements AdapterView.On
                     mToast = Toast.makeText(ConfirmActivity.this, "This function is currently not supported yet",
                             Toast.LENGTH_SHORT);
                     mToast.show();
-                    /*boolean noRecommend = true;
+                    boolean noRecommend = true;
                     if(recommendations != null) {
                         if (recommendations.size() > 0) {
                             noRecommend = false;
@@ -179,7 +178,7 @@ public class ConfirmActivity extends AppCompatActivity implements AdapterView.On
                                     Toast.LENGTH_SHORT);
                             mToast.show();
                         }
-                    }*/
+                    }
 
 
                 } else {
@@ -189,9 +188,9 @@ public class ConfirmActivity extends AppCompatActivity implements AdapterView.On
                     mToast.show();
                 }
             }
-        });
+        });*/
 
-        Button chooseRecommendButton = findViewById(R.id.button_confirm_recommend);
+        Button chooseRecommendButton = findViewById(R.id.button_confirm_stand);
         chooseRecommendButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -210,7 +209,7 @@ public class ConfirmActivity extends AppCompatActivity implements AdapterView.On
                 }
                 if(noRecommend) {
                     if (mToast != null) mToast.cancel();
-                    mToast = Toast.makeText(ConfirmActivity.this, "No recommendation available.",
+                    mToast = Toast.makeText(ConfirmActivity.this, "No stands available.",
                             Toast.LENGTH_SHORT);
                     mToast.show();
                 }
@@ -231,7 +230,7 @@ public class ConfirmActivity extends AppCompatActivity implements AdapterView.On
         //360 is value for location unknown
         double latitude = 360;
         double longitude = 360;
-        if(lastLocation != null) {
+        if (lastLocation != null) {
             latitude = lastLocation.getLatitude();
             longitude = lastLocation.getLongitude();
         }
@@ -257,7 +256,7 @@ public class ConfirmActivity extends AppCompatActivity implements AdapterView.On
         // TODO: to remove the following, when server can handle updated CommonOrderItem
         try {
             JSONArray array = jsonOrder.getJSONArray("orderItems");
-            for(int i =0 ; i<array.length(); i++){
+            for (int i = 0; i < array.length(); i++) {
                 JSONObject item = (JSONObject) array.get(i);
                 item.remove("price");
             }
@@ -271,33 +270,29 @@ public class ConfirmActivity extends AppCompatActivity implements AdapterView.On
 
         // Request recommendation from server for sent order (both in JSON)
         JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, url, jsonOrder,
-                                                            new Response.Listener<JSONObject>() {
+                new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        ObjectMapper mapper= new ObjectMapper();
+                        ObjectMapper mapper = new ObjectMapper();
                         //mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
                         try {
-                            recommendations= mapper.readValue(response.get("recommendations").toString(), new TypeReference<List<Recommendation>>() {});
-                            orderReceived= mapper.readValue(response.get("order").toString(), CommonOrder.class);
+                            recommendations = mapper.readValue(response.get("recommendations").toString(),
+                                    new TypeReference<List<Recommendation>>() {});
+                            orderReceived = mapper.readValue(response.get("order").toString(), CommonOrder.class);
                             orderReceived.setTotalPrice(totalPrice);
                             orderReceived.setPrices(ordered);
                             orderReceived.setTotalCount(cartCount);
                             // TODO: add ALL menuItem information to the orderItems!
 
-                            // Set expected time for order
-                            // timestamp in seconds, calendar in milliseconds
-                            if (recommendations.size() > 0) {
-                                long timestamp = recommendations.get(0).getTimeEstimate()*1000;
-                                timestamp += orderReceived.getStartTime().getTimeInMillis();
-                                GregorianCalendar cal = new GregorianCalendar();
-                                cal.setTimeInMillis(timestamp);
-                                orderReceived.setExpectedTime(cal);
+                            // Add recommendation stands to the spinner
+                            if (recommendations.size() > 0) standListAdapter.remove("No stands available");
+                            for (Recommendation i : recommendations) {
+                                standListAdapter.add(i.getStandName());
                             }
-
-                            showRecommendation();
+                            showRecommendation(0);
 
                         } catch (JsonProcessingException | JSONException e) {
-                            Log.v("JSON exception","JSON exception in confirmActivity");
+                            Log.v("JSON exception", "JSON exception in confirmActivity");
                         }
                     }
                 }, new Response.ErrorListener() {
@@ -310,8 +305,9 @@ public class ConfirmActivity extends AppCompatActivity implements AdapterView.On
             }
         }) { // Add JSON headers
             @Override
-            public @NonNull Map<String, String> getHeaders() {
-                Map<String, String>  headers  = new HashMap<>();
+            public @NonNull
+            Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
                 headers.put("Content-Type", "application/json");
                 headers.put("Authorization", user.getAuthorizationToken());
                 return headers;
@@ -323,93 +319,38 @@ public class ConfirmActivity extends AppCompatActivity implements AdapterView.On
     }
 
     /**
-     * Function to fetch the stand names from the server
-     */
-    public void fetchStandNames() {
-        // Instantiate the RequestQueue
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url = ServerConfig.OM_ADDRESS + "/stands";
-
-        // Request the stand names in JSON from the order manager
-        // Handle no network connection or server not reachable
-        JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET, url, null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-
-                        try {
-                            standListAdapter.clear();
-                            for (Iterator<String> iter = response.keys(); iter.hasNext(); ) {
-                                String standName = iter.next();
-                                String brandName = response.getString(standName);
-                                brandStandMap.put(brandName, standName);
-                            }
-                            // Only stands of the brand selected in the order can be shown
-                            for (String s : brandStandMap.get(ordered.get(0).getBrandName())) {
-                                standListAdapter.add(s);
-                            }
-
-                        } catch (Exception e) { // Catch all exceptions TODO: only specific ones
-                            Log.v("Exception fetchMenu", e.toString());
-                            if (mToast != null) mToast.cancel();
-                            mToast = Toast.makeText(ConfirmActivity.this, "An error occurred when fetching the stands!",
-                                    Toast.LENGTH_LONG);
-                            mToast.show();
-                        }
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                // NoConnectionError = no network connection0
-                // other = server not reachable
-                if (mToast != null) mToast.cancel();
-                if (error instanceof NoConnectionError) {
-                    mToast = Toast.makeText(ConfirmActivity.this, "No network connection",
-                            Toast.LENGTH_LONG);
-                    mToast.show();
-                } else {
-                    mToast = Toast.makeText(ConfirmActivity.this, "Server cannot be reached. No stands available.",
-                            Toast.LENGTH_LONG);
-                    mToast.show();
-                }
-            }
-        }) { // Add JSON headers
-            @Override
-            public @NonNull
-            Map<String, String> getHeaders()  {
-                Map<String, String>  headers  = new HashMap<>();
-                headers.put("Authorization", user.getAuthorizationToken());
-                return headers;
-            }
-        };
-
-        // Add the request to the RequestQueue
-        queue.add(jsonRequest);
-    }
-
-    /**
      * Function that updates the textViews to show the received recommendation info
+     * @param recommend the number of the recommendation in the recommendation list
      */
     @SuppressLint("SetTextI18n")
-    public void showRecommendation() {
+    public void showRecommendation(int recommend) {
         if(recommendations != null) {
-            if(recommendations.size() > 0) {
-                TextView recommend = findViewById(R.id.stand_recommend_text);
-                recommend.setText(recommendations.get(0).getStandName());
-                recommend.setTypeface(recommend.getTypeface(), Typeface.BOLD);
+            if (recommendations.size() > 0) {
+                // Set expected time for order
+                // timestamp in seconds, calendar in milliseconds
+                long timestamp = recommendations.get(recommend).getTimeEstimate() * 1000;
+                timestamp += orderReceived.getStartTime().getTimeInMillis();
+                GregorianCalendar cal = new GregorianCalendar();
+                cal.setTimeInMillis(timestamp);
+                orderReceived.setExpectedTime(cal);
+
+                // Display the chosen recommendation from the recommendation list
+                TextView recommendText = findViewById(R.id.stand_recommend_text);
+                recommendText.setText(recommendations.get(recommend).getStandName());
+                recommendText.setTypeface(recommendText.getTypeface(), Typeface.BOLD);
 
                 TextView distanceText = findViewById(R.id.recommend_distance_text);
                 distanceText.setVisibility(View.VISIBLE);
 
                 TextView distance = findViewById(R.id.recommend_distance);
-                distance.setText(Math.round(recommendations.get(0).getDistance()) + " meter");
+                distance.setText(Math.round(recommendations.get(recommend).getDistance()) + " meter");
                 distance.setVisibility(View.VISIBLE);
 
                 TextView remainingTimeText = findViewById(R.id.recommend_time_text);
                 remainingTimeText.setVisibility(View.VISIBLE);
 
                 TextView remainingTime = findViewById(R.id.recommend_time);
-                remainingTime.setText(recommendations.get(0).getTimeEstimate()/60 + " minute(s)");
+                remainingTime.setText(recommendations.get(recommend).getTimeEstimate()/60 + " minute(s)");
                 remainingTime.setVisibility(View.VISIBLE);
             }
         }
@@ -457,7 +398,11 @@ public class ConfirmActivity extends AppCompatActivity implements AdapterView.On
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
         // An item was selected in the spinner
-        chosenStand = (String) adapterView.getItemAtPosition(i);
+        if (!adapterView.getItemAtPosition(i).equals("No stands available")) {
+            chosenStand = (String) adapterView.getItemAtPosition(i);
+            showRecommendation(i);
+        }
+
     }
 
     @Override
