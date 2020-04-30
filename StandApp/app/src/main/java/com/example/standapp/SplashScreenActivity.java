dabb463b405b266a9050d41852b33fa4e158e999
Elberd <elberdgalaiev@gmail.com>
2020-04-30 22:48:56 +0200

    Fix same splash screen size for StandApp as for AttendeeApp

--- StandApp/app/src/main/java/com/example/standapp/SplashScreenActivity.java
+++ StandApp/app/src/main/java/com/example/standapp/SplashScreenActivity.java
@@ -2,23 +2,44 @@ package com.example.standapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-import gr.net.maroulis.library.EasySplashScreen;
+import com.jakewharton.threetenabp.AndroidThreeTen;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EasySplashScreen config = new EasySplashScreen(SplashScreenActivity.this)
+        AndroidThreeTen.init(this);
+        setContentView(R.layout.splash_screen);
+
+        Thread loading = new Thread() {
+            public void run() {
+                try {
+                    sleep(1500);
+                    Intent main = new Intent(SplashScreenActivity.this, MainActivity.class);
+                    startActivity(main);
+                    finish();
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    finish();
+                }
+            }
+        };
+        loading.start();
+
+        /*EasySplashScreen config = new EasySplashScreen(SplashScreenActivity.this)
                 .withFullScreen()
                 .withTargetActivity(MainActivity.class)
                 .withSplashTimeOut(2500)
                 .withLogo(R.mipmap.ic_launcher_foreground);
 
         View easySplashScreen = config.create();
-        setContentView(easySplashScreen);
+        setContentView(easySplashScreen);*/
     }
 }
