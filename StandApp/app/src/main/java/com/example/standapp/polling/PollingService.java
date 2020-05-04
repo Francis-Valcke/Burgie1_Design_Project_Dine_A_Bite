353159d635e13351d78f42592990ef5973df7db1
Elberd <elberdgalaiev@gmail.com>
2020-05-04 13:39:06 +0200

    Fix notifications in standApp

--- StandApp/app/src/main/java/com/example/standapp/polling/PollingService.java
+++ StandApp/app/src/main/java/com/example/standapp/polling/PollingService.java
@@ -1,8 +1,16 @@
 package com.example.standapp.polling;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
@@ -10,6 +18,8 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.android.volley.Request;
@@ -18,6 +28,8 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.standapp.OrderFragment;
+import com.example.standapp.R;
 import com.example.standapp.ServerConfig;
 import com.example.standapp.data.LoginDataSource;
 import com.example.standapp.data.LoginRepository;
@@ -41,6 +53,10 @@ public class PollingService extends Service {
 
     private Context context;
     private Handler handler;
+    private NotificationManagerCompat notificationManager;
+
+    public static final String CHANNEL_ORDER_ID = "incomingOrder";
+    private int notificationID = 0;
 
     public static final long DEFAULT_SYNC_INTERVAL = 10 * 1000;
 
@@ -84,6 +100,36 @@ public class PollingService extends Service {
                             e.printStackTrace();
                         }
                     }
+
+                    if (response.length() != 0) {
+                        // Send Notification that order is being prepared
+                        notificationManager = NotificationManagerCompat.from(context);
+                        // Create an Intent for the activity you want to start
+                        Intent activityIntent = new Intent(getApplication(), OrderFragment.class);
+                        // Create the TaskStackBuilder and add the intent, which inflates the back stack
+                        //TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+                        //stackBuilder.addNextIntentWithParentStack(activityIntent);
+                        // Get the PendingIntent containing the entire back stack
+                        //PendingIntent resultPendingIntent =
+                        //        stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);"""
+                        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, activityIntent, 0);
+                        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher_foreground);
+
+                        NotificationCompat.Builder notification = new NotificationCompat.Builder(context, CHANNEL_ORDER_ID)
+                                .setSmallIcon(R.mipmap.ic_launcher_foreground)
+                                .setContentTitle("New Order")
+                                .setContentText("You received a new order!")
+                                .setLargeIcon(largeIcon)
+                                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                                .setCategory(NotificationCompat.CATEGORY_STATUS)
+                                .setColor(Color.GREEN)
+                                // Set the intent that will fire when the user taps the notification
+                                .setContentIntent(contentIntent)
+                                .setAutoCancel(true);
+                        // notificationId is a unique int for each notification that you must define
+                        notificationManager.notify(notificationID, notification.build());
+                        notificationID++;
+                    }
                 }
             }, new Response.ErrorListener() {
                 @Override
@@ -114,6 +160,7 @@ public class PollingService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         context = getBaseContext();
+        createNotificationChannels();
         handler = new Handler();
 
         boolean ret = handler.post(runnableService);
@@ -138,6 +185,24 @@ public class PollingService extends Service {
         handler.removeCallbacks(runnableService);
         stopSelf();
         super.onDestroy();
+    }
+
+    /**
+     *  The notification settings here are just the default settings,
+     *  but the user has ultimate control over these settings and can disable them whenever he wants
+     */
+    private void createNotificationChannels() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel orderStart = new NotificationChannel(
+                    CHANNEL_ORDER_ID,
+                    "Incoming Order",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            orderStart.setDescription("This will notify you when you have an incoming order");
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(orderStart);
+        }
     }
 
 }
