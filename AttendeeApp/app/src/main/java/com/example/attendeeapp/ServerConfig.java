0cde5096f561847d534a536a3fedaa9000944a0e
Francis Valcke <francis.valcke@ugent.be>
2020-05-05 14:56:18 +0200

    Prepare for merge

--- AttendeeApp/app/src/main/java/com/example/attendeeapp/ServerConfig.java
+++ AttendeeApp/app/src/main/java/com/example/attendeeapp/ServerConfig.java
@@ -2,16 +2,16 @@ package com.example.attendeeapp;
 
 public class ServerConfig {
 
-    //public static final String OM_ADDRESS = "http://cobol.idlab.ugent.be:8091";
+    public static final String OM_ADDRESS = "http://cobol.idlab.ugent.be:8091";
+
+    public static final String EC_ADDRESS = "http://cobol.idlab.ugent.be:8093";
+
+    public static final String AS_ADDRESS = "http://cobol.idlab.ugent.be:8090";
     //
-    //public static final String EC_ADDRESS = "http://cobol.idlab.ugent.be:8093";
+    //public static final String OM_ADDRESS = "http://10.0.2.2:8081";
     //
-    //public static final String AS_ADDRESS = "http://cobol.idlab.ugent.be:8090";
-
-    public static final String OM_ADDRESS = "http://10.0.2.2:8081";
-
-    public static final String EC_ADDRESS = "http://10.0.2.2:8083";
-
-    public static final String AS_ADDRESS = "http://10.0.2.2:8080";
+    //public static final String EC_ADDRESS = "http://10.0.2.2:8083";
+    //
+    //public static final String AS_ADDRESS = "http://10.0.2.2:8080";
 
 }
